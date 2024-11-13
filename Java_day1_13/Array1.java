92530b819985fc725145f273aae5917bae872252
Sunidhibp <sunidhibp2003@gmail.com>
2024-11-13 14:21:44 +0530

    created basic program for initializing an array

---
+++ Java_day1_13/Array1.java
@@ -0,0 +1,17 @@
+public class Array1{
+    public static void main(String args[]){
+        int a[] = new int[5];
+        a[0] = 70;
+        a[1] = 10;
+        a[2] = 20;
+        a[3] = 30;
+        a[4] = 50;
+        for(int i =0;i<a.length; i++){
+
+            System.out.println(a[i]);
+        }
+
+
+    }
+
+}
